9b56e028585232bf7e132be41c2536cc15dc57e1
nxl-krishna <krishnarathore2792005@gmail.com>
2025-01-25 19:50:30 +0530

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -77,7 +77,7 @@ export const projects = [
     title: "full stack Cloud saving Notebook -ZENBOOK",
     des: "It is the cloud based platform to save your notes online with use of login sigin power to add,delete update notes anytime used react,mongodb,express to accomplish this project",
     img: "/p11111.png",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    iconLists: ["/re.svg", "/boot.png", "/js.png"],
     link: "/ui.earth.com",
   },
   {
@@ -85,7 +85,7 @@ export const projects = [
     title: "Categorized News website-NewsMonkey",
     des: "Simplified platform to read the all categories news online at single platform used api for the news in the website",
     img: "/p11.png",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+    iconLists: ["/re.svg", "/boot.png"],
     link: "/ui.yoom.com",
   },
   {
@@ -93,7 +93,7 @@ export const projects = [
     title: "College Website IIITV -Renovated",
     des: "this project  works on the HTML,CSS,javascript a simplified college website with responsive platform  ",
     img: "/p33.png",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+    iconLists: ["/html.png", "/css.png", "/js.png"],
     link: "/ui.aiimg.com",
   },
   {
@@ -101,7 +101,7 @@ export const projects = [
     title: "TextUtil-A App TO Modify Your phrases",
     des: "here you can write a text as long as you can copy,convert to capital or small",
     img: "/p66.png",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    iconLists: ["/re.svg", "/boot.png", "/js.png"],
     link: "/ui.apple.com",
   },
 ];
